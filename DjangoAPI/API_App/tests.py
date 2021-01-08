b9100ff7a2d797fa298dd4bccdccd00a7328dff3
Diego Alceb <>
2021-01-07 19:45:43 -0500

    API with GET, PUT, GET_BY_ID methods and tests

--- DjangoAPI/API_App/tests.py
+++ DjangoAPI/API_App/tests.py
@@ -1,34 +1,47 @@
 from django.test import TestCase, Client
+from .models import Article
+import json
 
 # Create your tests here.
 
 class APITestCase(TestCase):
+
+    ###### Server side test
+    
+    def setUp(self):
+        #Create some articles
+        Article.objects.create(name = "ART1", description = "DESC1")
+        Article.objects.create(name = "ART2", description = "DESC2")
+    
+    def test_get_all_articles(self):
+        self.assertEqual(Article.objects.all().count(), 2)
+    
+    ###### Client side test
+
     #GET ALL ARTICLES
     def test_api_get(self):
         client = Client()
         response = client.get("/api/articles")
         self.assertEqual(response.status_code, 200)
+        responsejson = json.loads(response.json()['articles'])
+        self.assertEqual(len(responsejson), 2)
+        self.assertEqual(responsejson[0]['fields']['name'], "ART1")
+        self.assertEqual(responsejson[0]['fields']['description'], "DESC1")
+        self.assertEqual(responsejson[1]['fields']['name'], "ART2")
+        self.assertEqual(responsejson[1]['fields']['description'], "DESC2")
 
     #PUT A ARTICLE
     def test_api_post(self):
         client = Client()
-        response = client.post("/api/articles")
-        self.assertEqual(response.status_code, 200)
+        response = client.post("/api/articles", {'name': 'ART3', 'description': 'DESC3'}, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
 
     #GET AN ARTICLE BY ID
     def test_api_get_by_id(self):
         client = Client()
         response = client.get("/api/articles/1")
         self.assertEqual(response.status_code, 200)
-
-    #UPDATE AN ARTICLE BY ID
-    def test_api_update_by_id(self):
-        client = Client()
-        response = client.put("/api/articles/1")
-        self.assertEqual(response.status_code, 200)
-
-    #DELETE AN ARTICLE BY ID
-    def test_api_delete_by_id(self):
-        client = Client()
-        response = client.delete("/api/articles/1")
-        self.assertEqual(response.status_code, 200)
+        responsejson = json.loads(response.json()['article'])
+        self.assertEqual(len(responsejson), 1)
+        self.assertEqual(responsejson[0]['fields']['name'], "ART1")
+        self.assertEqual(responsejson[0]['fields']['description'], "DESC1")
--- DjangoAPI/API_App/views.py
+++ DjangoAPI/API_App/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Article
+from django.core import serializers   
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def articles(request):
 
     if request.method == 'GET':
+        articles = Article.objects.all()
         return JsonResponse({
-            "articles" : "retuned all articles"
+            "articles" : serializers.serialize('json',articles)
         })
     elif request.method == 'POST':
-        return JsonResponse({
-            "articles" : "article inserted"
-        })
+        data = json.loads(request.body)
+        new_article = Article.objects.create(name = data['name'], description = data['description'])
+        if(new_article != None):
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=404)
     
 def article(request, article_id):
 
     if request.method == 'GET':
+        article = Article.objects.filter(pk = article_id)
         return JsonResponse({
-            "article found :" : article_id
+            "article" : serializers.serialize('json',article)
         })
-    elif request.method == 'PUT':
-        return JsonResponse({
-            "article updated :" : article_id
-        })
-    elif request.method == 'DELETE':
-        return JsonResponse({
-            "article deleted :" : article_id
-        })
+  
     
 
